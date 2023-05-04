db71ebb3c2f0bc6ee0f90624f7ee61c29431985b
Vitor <vitor.daniel@outlook.pt>
2023-05-04 17:47:07 +0100

    created GameUnits

---
+++ GameUnits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
