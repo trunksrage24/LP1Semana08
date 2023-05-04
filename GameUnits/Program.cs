39ffb1f24eb6d192a04cc0576902dbad5af2b694
Vitor <vitor.daniel@outlook.pt>
2023-05-04 17:54:20 +0100

    adde Program to GameUnits

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -2,11 +2,35 @@
 
 namespace GameUnits
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Criar array com três unidades
+            Unit[] units = new Unit[]
+            {
+                new MilitaryUnit(3, 10, 5),
+                new MilitaryUnit(4, 5, 3),
+                new SettlerUnit(),
+            };
+
+            // Unidade 0 ataca unidade 1
+            (units[0] as MilitaryUnit).Attack(units[1]);
+            // Unidade 0 ataca unidade 2
+            (units[0] as MilitaryUnit).Attack(units[2]);
+
+            // "Imprimir" cada unidade
+            // chamando implicitamente o método ToString() de cada uma delas
+            foreach (Unit u in units)
+            {
+                Console.WriteLine(u);
+            }
+
+            // Output esperado:
+            //
+            // MilitaryUnit: HP=12 COST=7 AP=5 XP=2
+            // MilitaryUnit: HP=4 COST=3 AP=3 XP=0
+            // SettlerUnit: HP=1 COST=5
         }
     }
 }
