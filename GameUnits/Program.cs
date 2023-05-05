c52c9cdd4f1f18db98f100347aceb1d2fedce09a
Vitor <vitor.daniel@outlook.pt>
2023-05-05 21:58:48 +0100

    fix errors

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,21 +6,19 @@ namespace GameUnits
     {
         private static void Main()
         {
-            // Create array with 3 units
             Unit[] units = new Unit[]
             {
                 new MilitaryUnit(3, 10, 5),
                 new MilitaryUnit(4, 5, 3),
                 new SettlerUnit(),
+                new SpyUnit(),
+                new SpyUnit()
             };
-            foreach (Unit u in units)
-            {
-                Console.WriteLine(u);
-            }
-            // Unit 0 atack unit 1
-            (units[0] as MilitaryUnit).Attack(units[1]);
-            // Unit 0 atack unit 2
+
+            (units[0] as MilitaryUnit).Attack(units[4]);
             (units[0] as MilitaryUnit).Attack(units[2]);
+            (units[3] as SpyUnit).GetSecretInfo(units[4]);
+            (units[4] as SpyUnit).GetSecretInfo(units[2]);
 
             foreach (Unit u in units)
             {
