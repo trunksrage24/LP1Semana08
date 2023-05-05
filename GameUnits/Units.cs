0ce57c4aeb04dc72d103d1fb47ea65c689e29a3a
Vitor <vitor.daniel@outlook.pt>
2023-05-05 20:43:02 +0100

    add Unit class

--- GameUnits/Units.cs
+++ GameUnits/Units.cs
@@ -3,8 +3,20 @@ using System;
 
 namespace GameUnits
 {
-    public class Units
+    public abstract class Unit
     {
-        
+        public int movement;
+        public virtual int Health { get; set; }
+        public abstract float Cost { get; }
+
+        public Unit (int movement, int health)
+        {
+            this.movement = movement;
+            Health = health;
+        }
+        public void Move()
+        {
+            Console.WriteLine("Movement: " + movement);
+        }
     }
 }
